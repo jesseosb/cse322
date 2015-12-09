229a158ffa9165aef1ba52390ee03a896a0f1b64
jesseosb <jesse618@live.com>
2015-12-09 15:07:32 -0800

    fixed

--- osrshstable.php
+++ osrshstable.php
@@ -21,40 +21,5 @@ foreach($skills as $value){
         </tr>";
     $i++;
 }
-// $responseTable = "<div class='row'>
-//         <div class='col-md-6'>
-//           <table class='table'>
-//             <thead>
-//               <tr>
-//                 <th>Skill</th>
-//                 <th>Rank</th>
-//                 <th>Level</th>
-//                 <th>XP</th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               <tr>
-//                 <td>Overall</td>
-//                 <td><span id='hs00'></span></td>
-//                 <td><span id='hs01'></span></td>
-//                 <td><span id='hs02'></span></td>
-//               </tr>
-//               <tr>
-//                 <td>Attack</td>
-//                 <td><span id='hs10'></span></td>
-//                 <td><span id='hs11'></span></td>
-//                 <td><span id='hs12'></span></td>
-//               </tr>
-//               <tr>
-//                 <td>Defence</td>
-//                 <td><span id='hs20'></span></td>
-//                 <td><span id='hs21'></span></td>
-//                 <td><span id='hs22'></span></td>
-//               </tr>
-//             </tbody>
-//           </table>
-//         </div>
-//       </div>";
-// echo json_encode($stats);
-// echo $responseTable;
+
 ?>
