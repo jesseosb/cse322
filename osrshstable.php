8145a663f79e989cc1da8591c7cb4c431d880af1
jesseosb <jesse618@live.com>
2015-12-09 15:54:39 -0800

    sdf

--- osrshstable.php
+++ osrshstable.php
@@ -4,6 +4,7 @@ $hs = file_get_contents("http://services.runescape.com/m=hiscore_oldschool/index
 $hs = explode("\n",$hs);
 $skills = array("Overall","Attack","Defence","Strength","Constitution","Ranged","Prayer","Magic","Cooking","Woodcutting","Fletching","Fishing","Firemaking","Crafting","Smithing","Mining","Herblore","Agility","Thieving","Slayer","Farming","Runecrafting","Hunter","Construction");
 $i = 0;
+$responseTxt
 
 foreach($skills as $value){
     $hs[$i] = explode(",",$hs[$i]);
@@ -13,7 +14,8 @@ foreach($skills as $value){
     $level = $hs[$i][1];
     $stats[$value]["xp"] = $hs[$i][2];
     $xp = $hs[$i][2];
-    echo "<tr><td>$value</td><td>$rank</td><td>$level</td><td>$xp</td></tr>";
+    responseTxt += "<tr><td>$value</td><td>$rank</td><td>$level</td><td>$xp</td></tr>";
     $i++;
 }
+echo responseTxt;
 ?>
